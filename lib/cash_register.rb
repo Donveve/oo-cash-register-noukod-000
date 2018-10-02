cf0db4bf61b2381fd14ea6d3785a61a27223d026
Veniel Dorestave <veniel@noukod.com>
2018-10-02 18:21:11 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,28 +1,31 @@
-class Register
-attr_accessor :total, :discount,
-:last_discount_amount, :item
 
-def initialize(discount = 0)
-@total = 0
-@discount = discount
-@item = []
-end
+class CashRegister
 
-def add_item(title, price, quantity = 1)
-  if quantity > 1
-        i = 0
-        while i < quantity
-          @items << title
-          i+=1
-        end
-      else
+  attr_accessor :total, :discount, :last_transaction_amount, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
         @items << title
+        i+=1
       end
-      @total += price*quantity
-      @last_transaction_amount = @total
-      @total
-end
-def apply_discount()
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
+
+  def apply_discount()
     if @discount > 0
       @discount = @discount/100.to_f
       @total = @total - (@total * (@discount))
